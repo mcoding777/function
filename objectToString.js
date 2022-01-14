90e2c99e6721d4389e4aebb3dbd5b0adeb833aac
misun <mcoding777@naver.com>
2022-01-14 13:24:07 +0900

    수정 : objectToString

--- objectToString.js
+++ objectToString.js
@@ -4,17 +4,21 @@
 // 인자 값 {'B3':'3', 'B1':'1', 'B2':'2', ...}
 
 function objectToString(object) {
-    const sort_object = {};
+    const array = [];
 
-    Object.keys(object).sort().forEach((key) => {
-        sort_object[key] = object[key];
-      }); // sort_object = {'B1':'1', 'B2':'2', 'B3':'3'...}
+    for (let i in object) {
+        array.push([i, object[i]]);
+    }
+
+    const sort_array = array.sort((a, b) => {
+      return a[1] - b[1]
+    }); // 반환 값 [['B1', '1'], ['B2', '2'], ['B3', '3'], ...]
     
     // 객체가 순서대로 있는 경우에는 여기서부터 사용!
     let string = "";
 
-    for (let i in sort_object) {
-        string = string + " " + `${i}=${sort_object[i]}`;
+    for (let j in sort_array) {
+        string = string + " " + `${sort_array[j][0]}=${sort_array[j][1]}`;
     }
 
     return string.trim(); // 처음에 오는 공백 제거
