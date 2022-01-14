a704cc6b1fe4a5505400bb4af15922f380629ebc
misun <mcoding777@naver.com>
2022-01-14 13:14:15 +0900

    객체를 문자열로 바꿔주는 함수 objectToString 추가

--- objectToString.js
+++ objectToString.js
@@ -1,13 +1,23 @@
-// 객체를 문자열로 바꿔주는 함수
-// 인자 값 {'B1':'1', 'B2':'2', 'B3':'3', ...}
-// 반환 값 "B1:1, B2:2, B3:3"
+// 객체를 value 기준으로 오름차순 정렬해서 문자열로 반환하는 함수
+// 객체가 순서대로 있지 않은 경우 처음부터 사용!
+
+// 인자 값 {'B3':'3', 'B1':'1', 'B2':'2', ...}
 
 function objectToString(object) {
-    let result = ""
+    const sort_object = {};
+
+    Object.keys(object).sort().forEach((key) => {
+        sort_object[key] = object[key];
+      }); // sort_object = {'B1':'1', 'B2':'2', 'B3':'3'...}
     
-    for (let i in object) {
-        result = result + " " + `${i}:${object[i]}`;
+    // 객체가 순서대로 있는 경우에는 여기서부터 사용!
+    let string = "";
+
+    for (let i in sort_object) {
+        string = string + " " + `${i}=${sort_object[i]}`;
     }
-    
-    return result.trim();
+
+    return string.trim(); // 처음에 오는 공백 제거
 }
+
+// 최종 반환 값 "B1=1 B2=2 B3=3 ..."
